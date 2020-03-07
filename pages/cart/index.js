5fd737f9bfd0f1e62617bbba5e12c130c58c6a1e
gaiz <594277665@qq.com>
2020-03-07 20:44:05 +0800

    完成极端总价

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -5,62 +5,133 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    address: {},
+    goods: [],
+    totalPrice: 0,
+    allCheck: true
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    //获取本地的收货地址
+    this.setData({
+      address: wx.getStorageSync('address') || []
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
+  onShow: function() {
+    //data和onload只执行一次，所以每次显示页面要获取本地地址
+    this.setData({
+      address: wx.getStorageSync('address') || []
+    })
+    // 需要获取商品详情本地数据
+    this.setData({
+      goods: wx.getStorageSync("goods") || []
+    });
+    this.handleAllPrice()
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  handleGetAddress() {
+    wx.chooseAddress({
+      success: (res) => {
+        this.setData({
+          address: {
+            name: res.userName,
+            tel: res.telNumber,
+            detail: res.provinceName + res.cityName + res.countyName + res.detailInfo
+          }
+        })
+        wx.setStorageSync('address', this.data.address);
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  cartChange(e) {
+    const {
+      index,
+      number
+    } = e.currentTarget.dataset;
+    //数量会变化，但是页面数量不会变化
+    this.data.goods[index].number += number
+    if (this.data.goods[index].number === 0) {
+      wx.showModal({
+        title: '提示',
+        content: '是否删除商品',
+        success: (res) => {
+          if (res.confirm) {
+            this.data.goods.splice(index, 1)
+          } else {
+            this.data.goods[index].number += 1
+          }
+          this.setData({
+            goods: this.data.goods
+          })
+        }
+      })
+    }
+    this.setData({
+      goods: this.data.goods
+    })
+    //数量变化计算总价格
+    this.handleAllPrice()
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  //点击选中按钮
+  handleCheck(e) {
+    const {
+      index
+    } = e.currentTarget.dataset;
+    const {
+      selct
+    } = this.data.goods[index]
+    this.data.goods[index].selct = !selct
+    this.setData({
+      goods: this.data.goods
+    })
+    this.handleAllPrice()
+    this.checkAllStatu()
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  //点击选中按钮时，全选按钮的状态
+  checkAllStatu() {
+    //先假设所有的商品都是选中状态
+    let handleAllCheck = true
+    this.data.goods.forEach(v => {
+      if (handleAllCheck === false) {
+        return
+      }
+      if (v.selct === false) {
+        handleAllCheck = false
+      }
+      this.setData({
+        allCheck: handleAllCheck
+      })
+    })
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //点击全选按钮
+  handleAllCheck() {
+    const {allCheck} = this.data
+    //每个商品的按钮被选
+    this.data.goods.forEach(v=>{
+      v.selct = !allCheck
+    })
+   
+    this.setData({
+      goods: this.data.goods,
+      allCheck: !allCheck
+    })
+  },
+  // 计算总价
+  handleAllPrice() {
+    let price = 0;
+    this.data.goods.forEach(v => {
+      if (v.selct) {
+        price += v.goods_price * v.number
+        this.setData({
+          totalPrice: price.toFixed(2)
+        })
+      }
+      wx.setStorageSync("goods", this.data.goods)
+    })
   }
 })
--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -6,8 +6,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    current:0,
-    detailInfo:{
+    current: 0,
+    detailInfo: {
 
     }
   },
@@ -15,27 +15,70 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
- request({
-   url:'/goods/detail',
-   data:{
-     goods_id:options.keywords
-   }
- }).then(res=>{
-   console.log(res)
-   const {message} = res.data
-   this.setData({
-     detailInfo : message
-   })
-   console.log(this.data.detailInfo)
- })
+  onLoad: function(options) {
+    request({
+      url: '/goods/detail',
+      data: {
+        goods_id: options.keywords
+      }
+    }).then(res => {
+      console.log(res)
+      const {
+        message
+      } = res.data
+      this.setData({
+        detailInfo: message
+      })
+      console.log(this.data.detailInfo)
+    })
   },
   //切换tab栏点击事件
-  clickTab(e){
+  clickTab(e) {
     console.log(e)
-  this.setData({
-    current: e.target.dataset.index
-  })
-    
+    this.setData({
+      current: e.target.dataset.index
+    })
+
+  },
+  handleToCart() {
+    wx.switchTab({
+      url: '/pages/cart/index',
+    })
+  },
+  clickAddcart() {
+    //点击加入购物车需要将此商品详情本地存储
+    //每次存储之前先获取本地商品，如果没有就是空数组
+    const goods = wx.getStorageSync("goods") || [];
+    //需要判断当前的商品是否已经在goods的数组中，存在就数量++
+    //some 循环数组，return的结果，只要有一个就返回true，反之就是false
+    const exit = goods.some(v => {
+      const isExit = v.goods_id === this.data.detailInfo.goods_id;
+      if (isExit) {
+        v.number += 1;
+        wx.showToast({
+          title: '数量+1',
+          icon: 'success',
+          duration: 2000
+        })
+      }
+      return isExit
+    })
+    //如果添加的商品数组中不存在
+    if(!exit){
+      goods.unshift({
+        goods_id: this.data.detailInfo.goods_id,
+        goods_name: this.data.detailInfo.goods_name,
+        goods_price: this.data.detailInfo.goods_price,
+        goods_small_logo: this.data.detailInfo.goods_small_logo,
+        number:1,//先默认数量是1
+        selct:true // 加入购物车默认是选中的状态
+      });
+      wx.showToast({
+        title: '加入成功',
+        icon: 'success',
+        duration: 2000
+      })
+    }
+    wx.setStorageSync("goods", goods);
   }
 })
